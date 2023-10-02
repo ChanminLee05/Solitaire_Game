357faf7ff8c34e51739d098978a5ab47a57367dd
unknown <lee00837@algonquinlive.com>
2023-10-02 16:46:04 -0600

    Completed SOL-16: Implement TableauCardStack logic.

--- src/com/cst8334/solitaire/cardstacks/TableauCardStack.java
+++ src/com/cst8334/solitaire/cardstacks/TableauCardStack.java
@@ -1,5 +1,15 @@
 package com.cst8334.solitaire.cardstacks;
 
-public class TableauCardStack {
+import java.util.List;
 
+import com.cst8334.solitaire.cards.Card;
+
+public class TableauCardStack extends CardStack {
+	public TableauCardStack() {
+		
+	}
+	
+	public TableauCardStack(List<Card> card) {
+		
+	}
 }
