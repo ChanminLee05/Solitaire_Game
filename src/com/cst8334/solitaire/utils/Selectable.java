e7ee8bd0d7edd6ef99426e3f7771c74e0332bd08
Mellodie9 <133682653+Mellodie9@users.noreply.github.com>
2023-10-12 11:27:14 -0400

    Update Selectable.java

--- src/com/cst8334/solitaire/utils/Selectable.java
+++ src/com/cst8334/solitaire/utils/Selectable.java
@@ -22,5 +22,22 @@ public interface Selectable {
    */
   public boolean isSelected();
 
+    /** Gets if a user clicked on the position of an entity
+   *
+   * @param position returns true if the position selected is assigned to an entity
+   * @return
+   */
+  public boolean contains (Position2D position);
+  
+  
+  /** Gets if a user selects a cardinal point where an entity is situated
+   * 
+   * @param x
+   * @param y
+   * @return
+   */
+  
+  public boolean contains (int x, int y);
+
 
 }
